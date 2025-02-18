47b6e3b713007934d9739fd539f9e03691185730
Iqbal <aodihis@gmail.com>
2025-02-18 22:44:07 +0700

    * Update port number and update the readme.

--- wsgi_win.py
+++ wsgi_win.py
@@ -4,4 +4,4 @@ from config import DevelopmentConfig
 app = create_app(DevelopmentConfig)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
